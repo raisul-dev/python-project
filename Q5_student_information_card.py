87d7727c07e6813a0b541e269cadf9fc3b2ed1aa
Md. Raisul Islam <remonmolla101@gmail.com>
2026-08-18 01:20:04 +0600

    Add files via upload

---
+++ Q5_student_information_card.py
@@ -0,0 +1,9 @@
+# take student information 
+student_name = input("Enter Your Name :")
+class_section = input("Enter Your Class Or Section :")
+school_name = input("Enter Your School Name :")
+
+# print student id card, i use f- string formate and \n for new line .
+print(f"-----STUDENT ID CARD -----\nName:{student_name}\nClass:{class_section}\nSchool:{school_name}")
+# use "-"for create borderS
+print("-"*26)
