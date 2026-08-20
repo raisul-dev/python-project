691b81734bd2eca31ad7a94d51af82e83daa12cf
Md. Raisul Islam <remonmolla101@gmail.com>
2026-08-20 16:32:10 +0600

    Add GPA grading logic based on input value
    Implement GPA grading system using if-else statements.

---
+++ find_gpa_using_if_else.py
@@ -0,0 +1,24 @@
+gpa = float (input("Enter Your GPA :"))
+
+if gpa < 0  or gpa > 4.00 :
+  print("invalid GPA")
+elif gpa == 4.00 :
+  print("A+")
+elif gpa >= 3.75:
+  print ("A")
+elif gpa >= 3.50 :
+  print("A-")
+elif gpa >= 3.25 :
+  print ("B+")
+elif gpa >= 3.00 :
+    print("B")
+elif gpa >= 2.75:
+    print("B-")
+elif gpa >= 2.50 :
+    print("C+")
+elif gpa >= 2.25 :
+    print("C")
+elif gpa >= 2.00:
+    print("D")
+else:
+    print("F")
